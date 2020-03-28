018d73553c49e8d28c811d9def773e56b4699c1c
Luis Herrera <herrera.lrh@gmail.com>
2020-03-28 20:10:26 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,14 @@
 class Appointment
+  @@all = []
 
+  def self.all 
+    @@all 
+  end
+
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient
+    @doctor = doctor
+  end
+  
 end
