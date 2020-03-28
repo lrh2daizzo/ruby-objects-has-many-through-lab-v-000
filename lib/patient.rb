35bc8aca3ad24be79a30863c39b5bda6ee8f8907
Luis Herrera <herrera.lrh@gmail.com>
2020-03-28 19:37:00 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,26 +1,3 @@
 class Patient
-  attr_accessor :name
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def new_appointment(doctor, date)
-  appointment = Appointment.new(date, self, doctor)
-end
-
-  def appointments
-    Appointment.all.select { |a| a.patient == self }
-  end
-
-  def doctors
-    appointments.map(&:doctor)
-  end
 
 end
